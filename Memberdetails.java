42740f50f035dcd0bbecde5fc149e8c0060daef3
anusha352 <72923019+anusha352@users.noreply.github.com>
2020-10-19 20:08:58 +0530

    Add files via upload
    Our group members list

---
+++ Memberdetails.java
@@ -0,0 +1,46 @@
+class Memberdetails
+{
+public static void main(String args[])
+{
+Details anusha = new Details();
+anusha.name = "anusha";
+anusha.place = "tarikere";
+anusha.college = "ait";
+anusha.hobbies = "watching mythological stories";
+anusha.groupname = "teamupgrade";
+anusha.information();
+
+Details arpitha = new Details();
+arpitha.name = "arpitha";
+arpitha.place = "antharagatte";
+arpitha.college = "ait";
+arpitha.hobbies = "listening music";
+arpitha.groupname = "teamupgrade";
+arpitha.information();
+
+Details chandru = new Details();
+chandru.name = "chandru";
+chandru.place = "gokak";
+chandru.college = "bgmit";
+chandru.hobbies = "reading newspaper";
+chandru.groupname = "teamupgrade";
+chandru.information();
+
+
+Details babitha = new Details();
+babitha.name = "babitha";
+babitha.place = "arasikere";
+babitha.college = "ait";
+babitha.hobbies = "singing";
+babitha.groupname = "teamupgrade";
+babitha.information();
+
+Details naheeda = new Details();
+naheeda.name = "naheeda";
+naheeda.place = "jamkhandi";
+naheeda.college = "bgmit";
+naheeda.hobbies = "watching cricket ";
+naheeda.groupname = "teamupgrade";
+naheeda.information();
+}
+}
